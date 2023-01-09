fbe445af105875ee82b2cdd0cc97c55ac40e82d0
LaupWing <loc_strict@hotmail.com>
2023-01-09 11:54:58 +0100

    noice

--- tutorial/src/routes/index.server.jsx
+++ tutorial/src/routes/index.server.jsx
@@ -1,3 +1,9 @@
 export default function Home() {
-   return <div>Hello World</div>
+   return (
+      <div>
+         <div>Hello World</div>
+         <p>Welcome to Hydrogen.</p>
+         <p>Hydrogen is a front-end web development framework used for building. Shopify custom storefronts</p>
+      </div>
+   )
 }
