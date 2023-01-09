6e556376d257f88db440bd58569f3b7ccdb5e830
LaupWing <loc_strict@hotmail.com>
2023-01-09 11:52:34 +0100

    update

--- tutorial/src/App.server.jsx
+++ tutorial/src/App.server.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import renderHydrogen from '@shopify/hydrogen/entry-server';
-import {Router, FileRoutes, ShopifyProvider} from '@shopify/hydrogen';
-import {Suspense} from 'react';
+import React from "react"
+import renderHydrogen from "@shopify/hydrogen/entry-server"
+import { Router, FileRoutes, ShopifyProvider } from "@shopify/hydrogen"
+import { Suspense } from "react"
 
 function App() {
-  return (
-    <Suspense fallback={null}>
-      <ShopifyProvider>
-        <Router>
-          <FileRoutes />
-        </Router>
-      </ShopifyProvider>
-    </Suspense>
-  );
+   return (
+      <Suspense fallback={null}>
+         <ShopifyProvider>
+            <Router>
+               <FileRoutes />
+            </Router>
+         </ShopifyProvider>
+      </Suspense>
+   )
 }
 
-export default renderHydrogen(App);
+export default renderHydrogen(App)
--- tutorial/vite.config.js
+++ tutorial/vite.config.js
@@ -1,6 +1,6 @@
-import {defineConfig} from 'vite';
-import hydrogen from '@shopify/hydrogen/plugin';
+import { defineConfig } from "vite"
+import hydrogen from "@shopify/hydrogen/plugin"
 
 export default defineConfig({
-  plugins: [hydrogen()],
-});
+   plugins: [hydrogen()],
+})
