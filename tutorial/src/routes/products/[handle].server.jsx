b552dfbf1e4daa091eccbaffdccf91fb45c918c8
LaupWing <loc_strict@hotmail.com>
2023-01-10 11:20:21 +0100

    udpated

---
+++ tutorial/src/routes/products/[handle].server.jsx
@@ -0,0 +1,16 @@
+import { useRouteParams } from "@shopify/hydrogen"
+import Layout from "../../components/Layout.server"
+
+const Product = () => {
+   const { handle } = useRouteParams()
+
+   return (
+      <Layout>
+         <section className="p-6 md:p-8 lg:p-12">
+            This will be teh product page for <strong>{handle}</strong>
+         </section>
+      </Layout>
+   )
+}
+
+export default Product
