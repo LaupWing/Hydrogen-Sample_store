a9fb6b13f3b143616dd8e35379fa99661e46f22f
LaupWing <loc_strict@hotmail.com>
2023-01-10 11:07:17 +0100

    niiiiiciu

--- tutorial/src/routes/collections/[handle].server.jsx
+++ tutorial/src/routes/collections/[handle].server.jsx
@@ -8,6 +8,7 @@ import {
 } from "@shopify/hydrogen"
 import { Suspense } from "react"
 import Layout from "../../components/Layout.server"
+import ProductCard from "../../components/ProductCard.server"
 
 const Collection = () => {
    const { handle } = useRouteParams()
@@ -51,6 +52,13 @@ const Collection = () => {
                </div>
             )}
          </header>
+         <section className="w-full gap-4 md:gap-8 grid p-6 md:p-8 lg:p-12">
+            <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+               {collection.products.nodes.map((product) => (
+                  <ProductCard key={product.id} product={product} />
+               ))}
+            </div>
+         </section>
       </Layout>
    )
 }
