093cc4698293beb71c521165b990d3146223f7cf
LaupWing <loc_strict@hotmail.com>
2023-01-11 12:07:07 +0100

    OptionRadio

--- tutorial/src/components/ProductDetails.client.jsx
+++ tutorial/src/components/ProductDetails.client.jsx
@@ -1,4 +1,4 @@
-import { MediaFile, ProductOptionsProvider } from "@shopify/hydrogen"
+import { MediaFile, ProductOptionsProvider, useProductOptions } from "@shopify/hydrogen"
 
 const ProductDetails = ({ product }) => {
    return (
@@ -35,6 +35,72 @@ const ProductDetails = ({ product }) => {
 }
 
 export default ProductDetails
+
+function ProductForm({ product }){
+   const { options, selectedVariant } = useProductOptions()
+   const isOutOfStock = !selectedVariant?.availableForSale || false
+
+   return (
+      <form className="grid gap-10">
+         <div className="grid gap-4">
+            {options.map(({name, values}) => {
+               if(values.length === 1){
+                  return null
+               }
+               return (
+                  <div 
+                     key={name}
+                     className="flex flex-wrap items-baseline justify-start gap-6"
+                  >
+                     <legend className="whitespace-pre-wrap max-w-prose font-bold text-lead min-w-w[4rem]">
+                        {name}
+                     </legend>
+                     <div className="flex flex-wrap items-baseline gap-4">
+                        <OptionRadio 
+                           name={name}
+                           values={values}
+                        />
+                     </div>
+                  </div>
+               )
+            })}
+         </div>
+      </form>
+   )
+}
+
+function OptionRadio({values, name}) {
+   const {selectedOptions, setSelectedOption} = useProductOptions()
+
+   return (
+      <>
+         {values.map(value => {
+            const checked = selectedOptions[name] === value
+            const id = `option-${name}-${value}`
+
+            return (
+               <label
+                  key={id}
+                  htmlFor={id}
+               >
+                  <input 
+                     type="radio" 
+                     id={id}
+                     className="sr-only"
+                     name={`option[${name}]`}
+                     value={value}
+                     checked={checked}
+                     onChange={() => setSelectedOption(name, value)}
+                  />
+                  <div className={`leading-none border-b-[2px] py-1 cursor-pointer transition-all duration-200 ${checked ? "border-gray-500" :"border-neutral-50"}`}>
+                     {value}
+                  </div>
+               </label>
+            )
+         })}
+      </>
+   )
+}
 
 function ProductGallery({ media }){
    if(!media.length){
