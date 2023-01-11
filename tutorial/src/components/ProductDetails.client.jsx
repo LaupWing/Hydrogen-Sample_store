3c70905d21d05bef0c515725a28eb0b785c12b7d
LaupWing <loc_strict@hotmail.com>
2023-01-11 11:48:03 +0100

    update

--- tutorial/src/components/ProductDetails.client.jsx
+++ tutorial/src/components/ProductDetails.client.jsx
@@ -1,4 +1,4 @@
-import { ProductOptionsProvider } from "@shopify/hydrogen"
+import { MediaFile, ProductOptionsProvider } from "@shopify/hydrogen"
 
 const ProductDetails = ({ product }) => {
    return (
@@ -7,17 +7,27 @@ const ProductDetails = ({ product }) => {
             <div className="grid items-start gap-6 lg:gap-20 md:grid-cols-2 lg:grid-cols-3">
                <div className="grid md:grid-flow-row md:p-0 md:overflow-x-auto md:grid-cols-2 md:w-full lg:col-span-2">
                   <div className="md:col-span-2 snap-center card-image aspect-square md:w-full w-[80vw] shadow rounded">
-
+                     <ProductGallery media={product.media.nodes}/>
                   </div>
                </div>
-            </div>
-            <div className="mt-8">
-               <div
-                  className="prose border-t border-gray-200 pt-6 text-black text-md"
-                  dangerouslySetInnerHTML={{
-                     __html: product.descriptionHtml
-                  }}
-               />
+               <div className="sticky md:mx-auto max-w-xl md:max-w-[24rem] grid gap-8 p-0 md:p-6 md:px-0 top-[6rem] lg:top-[8rem] xl:top-[10rem]">
+                  <div className="grid gap-2">
+                     <h1 className="text-4xl font-bold leading-10 whitespace-normal">
+                        {product.title}
+                     </h1>
+                     <span className="max-w-prone whitespace-pre-wrap inherit text-copy opacity-50 font-medium">
+                        {product.vendor}
+                     </span>
+                  </div>
+                  <div className="mt-8">
+                     <div
+                        className="prose border-t border-gray-200 pt-6 text-black text-md"
+                        dangerouslySetInnerHTML={{
+                           __html: product.descriptionHtml
+                        }}
+                     />
+                  </div>
+               </div>
             </div>
          </section>
       </ProductOptionsProvider>
@@ -25,3 +35,51 @@ const ProductDetails = ({ product }) => {
 }
 
 export default ProductDetails
+
+function ProductGallery({ media }){
+   if(!media.length){
+      return null;  
+   }
+
+   return (
+      <div className={`grid gap-4 overflow-x-hidden grid-flow-col md:grid-flow-row md:p-0 md:overflow-x-auto md:grid-cols-2 w-screen md:w-full lg:col-span-2`}>
+         {media.map((med, i) => {
+            let extraProps = {}
+
+            if(med.mediaContentType === "MODEL_3D"){
+               extraProps = {
+                  interactionPromptThreshold: "0",
+                  ar: true,
+                  loading: "eager",
+                  disableZoom: true
+               }
+            }
+
+            const data = {
+               ...med,
+               image: {
+                  ...med.image,
+                  altText: med.alt || "Product Image"
+               }
+            }
+
+            return (
+               <div
+                  className={`${ i % 3 == 0 ? "md:col-span-2" : "md:col-span-1"} snap-center card-image bg-white aspect-square md:w-full w-[80vw] shadow-sm rounded`}
+                  key={med.id || med.image.id}
+               >
+                  <MediaFile
+                     tabIndex="0"
+                     className={`w-full h-full aspect-square object-cover`}
+                     data={data}
+                     options={{
+                        crop: "center"
+                     }}
+                     {...extraProps}
+                  />
+               </div>
+            )
+         })}
+      </div>
+   )
+}
