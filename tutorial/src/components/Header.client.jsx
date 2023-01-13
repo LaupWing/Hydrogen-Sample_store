9d65bf36c0d5c28cb7005d353aa5842e27bbfe7e
LaupWing <loc_strict@hotmail.com>
2023-01-13 09:14:08 +0100

    cartd badge

--- tutorial/src/components/Header.client.jsx
+++ tutorial/src/components/Header.client.jsx
@@ -1,5 +1,5 @@
 import { Drawer, useDrawer } from "./Drawer.client"
-import { useUrl, Link } from "@shopify/hydrogen"
+import { useUrl, Link, useCart } from "@shopify/hydrogen"
 
 const Header = ({ shop }) => {
    const { pathname } = useUrl()
@@ -47,5 +47,22 @@ function IconBag() {
             d="M8.125 5a1.875 1.875 0 0 1 3.75 0v.375h-3.75V5Zm-1.25.375V5a3.125 3.125 0 1 1 6.25 0v.375h3.5V15A2.625 2.625 0 0 1 14 17.625H6A2.625 2.625 0 0 1 3.375 15V5.375h3.5ZM4.625 15V6.625h10.75V15c0 .76-.616 1.375-1.375 1.375H6c-.76 0-1.375-.616-1.375-1.375Z"
          />
       </svg>
+   )
+}
+
+function CartBadge({ dark }) {
+   const { totalQuantity } = useCart()
+
+   if (totalQuantity < 1) {
+      return null
+   }
+   return (
+      <div
+         className={`${
+            dark ? "text-black bg-white" : "text-white bg-black"
+         } absolute bottom-1 right-1 text-[0.625rem] font-medium subpixel-antialiased h-3 min-w-[0.75rem] flex items-center justify-center leading-none text-center rounded-full w-auto px-[0.125rem] pb-px`}
+      >
+         <span>{totalQuantity}</span>
+      </div>
    )
 }
