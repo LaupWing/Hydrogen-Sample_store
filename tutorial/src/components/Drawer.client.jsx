f5424a7d552941d269c184c316ae72e885a7628e
LaupWing <loc_strict@hotmail.com>
2023-01-13 08:59:51 +0100

    Transition

--- tutorial/src/components/Drawer.client.jsx
+++ tutorial/src/components/Drawer.client.jsx
@@ -21,6 +21,23 @@ const Drawer = () => {
             >
                <div className="fixed inset-0 bg-black bg-opacity-25"/>
             </Transition.Child>
+            <div className="fixed inset-0">
+               <div className="absolute insets-0 overflow-hidden">
+                  <div className="fixed inset-y-0 right-0 flex max-w-full pl-10">
+                     <Transition.Child
+                        as={Fragment}
+                        enter="transform transition ease-in-out duration-500 sm:duration-700"
+                        enterFrom="translate-x-full"
+                        enterTo="translate-x-0"
+                        leave="transform transition ease-in-out duration-500 sm:duration-700"
+                        leaveFrom="translate-x-0"
+                        leaveTo="translate-x-full"
+                     >
+                        
+                     </Transition.Child>
+                  </div>
+               </div>
+            </div>
          </Dialog>
       </Transition>
    )
