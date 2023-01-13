9bc7014351cd92aa1b849768efadb876e3395eae
LaupWing <loc_strict@hotmail.com>
2023-01-13 09:48:35 +0100

    OrderSummary

--- tutorial/src/components/CartDetails.client.jsx
+++ tutorial/src/components/CartDetails.client.jsx
@@ -72,3 +72,32 @@ const CartCheckoutActions = () => {
       </>
    )
 }
+
+const OrderSummary = () => {
+   const { cost } = useCart()
+
+   return (
+      <>
+         <dl className="space-y-2">
+            <div className="flex items-center justify-between">
+               <dt>Subtotal</dt>
+               <dd>
+                  {cost?.subtotalAmount?.amount ? (
+                     <Money data={cost?.subtotalAmount}/>
+                  ) :(
+                     "-"
+                  )}
+               </dd>
+            </div>
+            <div className="flex items-center justify-between">
+               <dt className="flex items-center">
+                  <span>Shipping estimate</span>
+               </dt>
+               <dd className="text-green-600">
+                  Free and carbon neutral
+               </dd>
+            </div>
+         </dl>
+      </>
+   )
+}
