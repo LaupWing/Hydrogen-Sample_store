c8a13c5075c7875ba7ca88a7cdbb1d07c4298675
LaupWing <loc_strict@hotmail.com>
2023-01-13 09:23:15 +0100

    cartdetails

---
+++ tutorial/src/components/CartDetails.client.jsx
@@ -0,0 +1,25 @@
+import {
+   useCart,
+   useCartLine,
+   CartLineProvider,
+   CartShopPayButton,
+   CartLineQuantityAdjustButton,
+   CartLinePrice,
+   CartLineQuantity,
+   Image,
+   Link,
+   Money,
+ } from "@shopify/hydrogen"
+
+
+const CartDetails = () => {
+   const { lines } = useCart()
+
+   if(lines.length === 0) {
+      return
+   }
+
+   return <div>CartDetails</div>
+}
+
+export default CartDetails
