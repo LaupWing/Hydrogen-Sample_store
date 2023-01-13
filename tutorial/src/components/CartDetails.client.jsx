a9d363814db153155dbfca383d9ca33054c80590
LaupWing <loc_strict@hotmail.com>
2023-01-13 09:29:42 +0100

    cart empty

--- tutorial/src/components/CartDetails.client.jsx
+++ tutorial/src/components/CartDetails.client.jsx
@@ -12,19 +12,31 @@ import {
  } from "@shopify/hydrogen"
 
 
-const CartDetails = () => {
+const CartDetails = ({ onClose }) => {
    const { lines } = useCart()
 
    if(lines.length === 0) {
-      return
+      return <CartEmpty onClose={onClose}/>
    }
 
-   return <div>CartDetails</div>
+   return (
+      <form></form>
+   )
 }
 
 export default CartDetails
 
 
 export const CartEmpty = ({ onClose }) => {
-
+   <div className="flex flex-col space-y-7 justify-center items-center md:py-8 md:px-12 px-4 py-6 h-screen">
+      <h2 className="whitespace-pre-wrap max-w-prose font-bold text-4xl">
+         Your cart is empty
+      </h2>
+      <button
+         onClick={onClose}
+         className="inline-block rounded-sm font-medium text-center py-3 px-6 max-w-xl leading-none bg-black text-white w-full"
+      >
+         Continue shopping
+      </button>
+   </div>
 }
