1dfffb5b012aba3e6c521c6ad8b3df2d213e4e48
LaupWing <loc_strict@hotmail.com>
2023-01-13 09:45:20 +0100

    CartCheckoutActions

--- tutorial/src/components/CartDetails.client.jsx
+++ tutorial/src/components/CartDetails.client.jsx
@@ -2,6 +2,7 @@ import {
    useCart,
    useCartLine,
    CartLineProvider,
+   
    CartShopPayButton,
    CartLineQuantityAdjustButton,
    CartLinePrice,
@@ -20,7 +21,20 @@ const CartDetails = ({ onClose }) => {
    }
 
    return (
-      <form></form>
+      <form className="grid grid-cols-1 grid-rows-[1fr_auto] h-[calc(100vh-6rem)]">
+         <section
+            aria-aria-labelledby="cart-contents"
+            className="px-4 pb-4 overflow-auto transition md:px-12"
+         >
+            <ul className="grid gap-6 md:gap-10 overflow-y-scroll">
+               {lines.map(line => (
+                  <CartLineProvider key={line.id} line={line}>
+                     
+                  </CartLineProvider>
+               ))}
+            </ul>
+         </section>
+      </form>
    )
 }
 
@@ -39,4 +53,22 @@ export const CartEmpty = ({ onClose }) => {
          Continue shopping
       </button>
    </div>
+}
+
+const CartCheckoutActions = () => {
+   const { checkoutUrl } = useCart()
+
+   return (
+      <>
+         <div className="flex flex-col items-center mt-6 md:mt-8">
+            <Link
+               to={checkoutUrl}
+               className="inline-block rounded-sm font-medium text-center py-3 px-6 max-w-xl leading-none bg-black text-white w-full"
+            >
+               Continue to Checkout
+            </Link>
+            <CartShopPayButton className="flex items-center justify-center rounded-sm mt-2 bg-[#5a31f4] w-full"/>
+         </div>
+      </>
+   )
 }
